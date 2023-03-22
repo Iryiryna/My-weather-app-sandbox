7fff04f179c570f998b2a8731af40853a3c50a46
Iryiryna <118272871+Iryiryna@users.noreply.github.com>
2023-03-22 21:28:40 +0200

    Updated

--- src/index.js
+++ src/index.js
@@ -62,6 +62,10 @@ form.addEventListener("submit", searchCity);
 function weatherCity(response) {
   let temperature = document.querySelector("#temperature");
   temperature.innerHTML = `${Math.round(response.data.main.temp)}`;
+  let detailsElement = document.querySelector("#details");
+  detailsElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+ 
 
   let h1 = document.querySelector("h1");
   h1.innerHTML = response.data.name;
